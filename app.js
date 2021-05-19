6b7b7792604f6f23277ad08e7b15a6f6ed6bb5c6
songyeon92 <76609152+songyeon92@users.noreply.github.com>
2021-05-20 00:00:25 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -5,19 +5,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res)=>{
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res)=> {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    
-    var week = ["Sunday", "Monday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var day = week[currentDay];
+    var options = { 
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric' 
+    };
 
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString("ja-JP", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        addItems: items
+    });
 });
 
+app.post("/", (req, res)=> {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Server is running at port 3000!");
